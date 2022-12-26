2bda8727ee98f77f31d863618f337abbdc46d659
Archana Vaideeswaran <46862596+ArchanaVaideeswaran@users.noreply.github.com>
2022-12-26 19:18:12 +0530

    Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../public/airdrop-logo.png";
+import HotAirBaloons from "../public/hot-air-baloons.png";
+import HomeStyles from "../styles/Home.module.css";
+
 export default function Home() {
-  return null;
+  return (
+    <div className={HomeStyles.root}>
+      <div className={HomeStyles.header}>
+        <Link href="/">
+            <div className={HomeStyles.logo}>
+              <Image
+                src={Logo}
+                alt="airdrop logo"
+                width={50}
+                height={50}
+              ></Image>
+              <h1>Airdrop</h1>
+            </div>
+        </Link>
+        <div>
+          <button>Connect Wallet</button>
+        </div>
+      </div>
+      <div className={HomeStyles.content}>
+        <div>
+          <Image
+            src={HotAirBaloons}
+            alt="hot air baloons"
+            width={200}
+            height={200}
+          ></Image>
+        </div>
+        <div>
+          <p>Connect wallet and claim your airdrop tokens!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
